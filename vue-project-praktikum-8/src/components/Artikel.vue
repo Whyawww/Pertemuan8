3eeeaadc3b778e528caf5e125b4a7349b8ff0160
unknown <wajinusantara05@gmail.com>
2024-05-30 14:54:30 +0700

    Vue js

---
+++ vue-project-praktikum-8/src/components/Artikel.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Presiden ke 7 Indonesia</h1>
+        <p>Ini adalah Presiden ke 7 Indonesia yang menduduki 2 periode namun awal awal memiliki banyak haters</p>
+    </div>
+    <img src="../assets/PresidenKita.jpg" alt="Presiden" width="400px" height="auto">
+</template>
